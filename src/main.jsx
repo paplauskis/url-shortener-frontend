7d0c02056ab3e4427f46bb70c957331cbf1b6a0a
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-12-18 16:54:27 +0200

    minor bug fix (.js removal)

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
-import { createRoot } from 'react-dom/client.js'
+import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
