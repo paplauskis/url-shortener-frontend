52dbf2c37076a087fe24ed29c1008dceb670e4e7
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-12-18 13:54:10 +0200

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import './App.css'
 
 function App() {
   return (
-    <>
-    </>
+    <Routes>
+      <Route path="/" element={} />
+      <Route path="/url" element={} />
+      <Route path="/stats" element={} />
+    </Routes>
   )
 }
 
