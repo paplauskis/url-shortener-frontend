75cffe1339330efd67fef26c1a23fb36404ced57
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-12-18 13:54:35 +0200

    added BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import { createRoot } from 'react-dom/client.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
