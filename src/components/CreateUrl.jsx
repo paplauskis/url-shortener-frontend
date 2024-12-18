e436f6417e02e8d4ac169947bf23309d8e76b957
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-12-18 20:38:00 +0200

    added logic to create url

---
+++ src/components/CreateUrl.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+function CreateUrl() {
+  const [originalUrl, setOriginalUrl] = useState('');
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await fetch(
+        `${import.meta.env.VITE_API_URL}/shorten/${encodeURIComponent(originalUrl)}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      
+      setOriginalUrl('')
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  
+  return (
+    <div className="create-url">
+      <h3>Type URL you want to shorten</h3>
+      <form onSubmit={handleSubmit}>
+        <div className="form-div">
+          <label>
+            Original URL
+          </label>
+          <input
+            type="url"
+            value={originalUrl}
+            onChange={(e) => setOriginalUrl(e.target.value)}
+            required
+          />
+          <button type="submit">Create Short URL</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default CreateUrl
